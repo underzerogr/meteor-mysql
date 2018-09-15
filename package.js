7484c1948ff29e73f0b648683254ecac2330e535
Vag <vag@radioland.gr>
2018-09-15 19:02:35 +0300

    Test es6

--- package.js
+++ package.js
@@ -1,61 +1,62 @@
 Package.describe({
-  name: 'numtel:mysql',
-  summary: 'MySQL support with Reactive Select Subscriptions',
-  version: '1.0.6',
-  git: 'https://github.com/numtel/meteor-mysql.git'
+	name: 'underzero:mysql',
+	summary: 'MySQL support with Reactive Select Subscriptions',
+	version: '2.0.0',
+	git: 'https://github.com/underzerogr/meteor-mysql'
 });
 
 Npm.depends({
-  'mysql': '2.10.2',
-  'mysql-live-select': '1.0.6'
+	'mysql': '2.10.2',
+	'mysql-live-select': '1.0.6'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use([
-    'underscore',
-    'ddp',
-    'tracker'
-  ]);
+Package.onUse(function (api) {
+	api.versionsFrom('1.0');
+	api.use([
+		'underscore',
+		'ddp',
+		'tracker',
+		'jquery'
+	]);
 
-  api.addFiles('lib/LiveMysql.js', 'server');
-  api.export('LiveMysql', 'server');
+	api.addFiles('lib/LiveMysql.js', 'server');
+	api.export('LiveMysql', 'server');
 
-  api.addFiles('lib/MysqlSubscription.js');
-  api.export('MysqlSubscription');
+	api.addFiles('lib/MysqlSubscription.js');
+	api.export('MysqlSubscription');
 });
 
-Package.onTest(function(api) {
-  api.use([
-    'tinytest',
-    'templating',
-    'underscore',
-    'autopublish',
-    'insecure',
-    'http',
-    'grigio:babel@0.1.1',
-    'simple:rest@1.1.1',
-    'numtel:mysql',
-  ]);
-  api.use('test-helpers'); // Did not work concatenated above
-  api.addFiles([
-    'test/helpers/expectResult.js',
-    'test/helpers/randomString.js'
-  ]);
+Package.onTest(function (api) {
+	api.use([
+		'tinytest',
+		'templating',
+		'underscore',
+		'autopublish',
+		'insecure',
+		'http',
+		'grigio:babel@0.1.1',
+		'simple:rest@1.1.1',
+		'numtel:mysql'
+	]);
+	api.use('test-helpers'); // Did not work concatenated above
+	api.addFiles([
+		'test/helpers/expectResult.js',
+		'test/helpers/randomString.js'
+	]);
 
-  api.addFiles([
-    'test/fixtures/tpl.html',
-    'test/fixtures/tpl.js'
-  ], 'client');
+	api.addFiles([
+		'test/fixtures/tpl.html',
+		'test/fixtures/tpl.js'
+	], 'client');
 
-  api.addFiles([
-    'test/helpers/queryEx.js',
-    'test/helpers/querySequence.js',
-    'test/index.es6'
-  ], 'server');
+	api.addFiles([
+		'test/helpers/queryEx.js',
+		'test/helpers/querySequence.js',
+		'test/index.es6'
+	], 'server');
 
-  api.addFiles([
-    'test/MysqlSubscription.js',
-    'test/simple_rest.js'
-  ]);
+	api.addFiles([
+		'test/MysqlSubscription.js',
+		'test/simple_rest.js'
+	]);
 });
